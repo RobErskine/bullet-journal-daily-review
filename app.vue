f2cac8f9762a7cac247304876a70d22bf9c92420
Rob Erskine <rob@roberskine.com>
2023-01-19 17:37:06 -0500

    adding in logic on when to post

--- app.vue
+++ app.vue
@@ -4,21 +4,22 @@
 
 <script>
 export default {
-  // data() {
-  //   return {
-  //     form: {
-  //       formName: "",
-  //       email: "",
-  //       message: "",
-  //     },
-  //   };
-  // },
+  data() {
+    return {
+      submitted: false
+    }
+  },
   methods: {
     currentDate() {
       const current = new Date();
       const months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
       const date = `${current.getFullYear()}-${months[current.getMonth()]}-${current.getDate()}`;
       return date;
+    },
+    currentTimeInHours() {
+      const current = new Date();
+      const hours = current.getHours();
+      return (hours - 12);
     },
     encode(data) {
       return Object.keys(data)
@@ -43,54 +44,85 @@ export default {
           'learn': document.getElementsByName('learn')[0].checked,
           'review': document.getElementById('review').value,
         })
-      }).then(() => { alert('success!') }).catch((error) => alert(error));
+      }).then(() => { 
+        // alert('success!')
+        this.submitted = true 
+      }).catch((error) => alert(error));
     }
   }
 }
 </script>
 
+<style>
+body{
+  margin:0px;
+  padding:0px;
+  height:100vh;
+}
+
+body > div{
+  height: inherit;
+}
+</style>
+
 <template>
-  <main class="p-4 w-full">
-    <form id="daily-review" data-netlify="true" method="POST" netlify name="bullet-journal" @submit.prevent="handleSubmit()">
-      <div>
-        <input id="date" type="hidden" name="date" :value=currentDate() >
-        
-        <div>
-          <h1 class="text-3xl text-center font-bold">
-            Daily Review
-          </h1>
-        </div>
+  <main class="p-4 w-full h-full block flex pb-24">
+    <div v-if="currentTimeInHours() < 10">
+      <div class="flex flex-col align-center justify-center h-full p-8">
+        <h1 class="text-center text-3xl font-bold">It's a tad too early to review today!</h1>
+        <p class="text-center text-md block mt-6">You cannot review today yet. <br/>Come back after 10pm.</p>
+      </div>
+    </div>
+    <div v-else>
+      <div v-if="this.submitted === false">
+        <form id="daily-review" data-netlify="true" method="POST" netlify name="bullet-journal" @submit.prevent="handleSubmit()">
+          <div>
+            <input id="date" type="hidden" name="date" :value=currentDate() >
+            
+            <div>
+              <h1 class="text-3xl text-center font-bold">
+                Daily Review
+              </h1>
+            </div>
 
-        <div class="block py-2">
-          <label for="numberreview" class="block text-sm font-medium text-gray-700 mb-2">How was your day?</label>
-          <input type="tel" name="numberreview" id="numberreview" class="block p-2 text-center w-full rounded-md border-2 border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" aria-describedby="numberreview-description">
-          <p class="mt-1 text-sm text-gray-500" id="numberreview-description">Use a 1-10 scale. 10 Being a great day!</p>
-        </div>
-        
-        <ToggleItem title="🇫🇷 Practice French?" description="Avez-vous étudié le français aujourd'hui? C'est en forgeant qu'on devient forgeron!" name="language" />
-      
-        <ToggleItem title="💪 Get some exercise?" description="Don't gotta be super buff but you can't have a beer gut either. Get your heart rate up today?" name="exercise" />
+            <div class="block py-2">
+              <label for="numberreview" class="block text-sm font-medium text-gray-700 mb-2">How was your day?</label>
+              <input type="tel" name="numberreview" id="numberreview" class="block p-2 text-center w-full rounded-md border-2 border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" aria-describedby="numberreview-description">
+              <p class="mt-1 text-sm text-gray-500" id="numberreview-description">Use a 1-10 scale. 10 Being a great day!</p>
+            </div>
+            
+            <ToggleItem title="🇫🇷 Practice French?" description="Avez-vous étudié le français aujourd'hui? C'est en forgeant qu'on devient forgeron!" name="language" />
+          
+            <ToggleItem title="💪 Get some exercise?" description="Don't gotta be super buff but you can't have a beer gut either. Get your heart rate up today?" name="exercise" />
 
-        <ToggleItem title="👬 Hang with friends?" description="Gettin' older makes it harder to meet new friends. Did you talk to another friendly soul?" name="friends" />
+            <ToggleItem title="👬 Hang with friends?" description="Gettin' older makes it harder to meet new friends. Did you talk to another friendly soul?" name="friends" />
 
-        <ToggleItem title="🎢 Jam on a personal project?" description="Nothing like building something with your own hands. Make some time to make something for fun?" name="personalproject" />
+            <ToggleItem title="🎢 Jam on a personal project?" description="Nothing like building something with your own hands. Make some time to make something for fun?" name="personalproject" />
 
-        <ToggleItem title="💡 Learn something new?" description="Stagnation is death! Keep your mind sharp! Did you get spend time flex a new skill?" name="learn" />
+            <ToggleItem title="💡 Learn something new?" description="Stagnation is death! Keep your mind sharp! Did you get spend time flex a new skill?" name="learn" />
 
-        <div>
-          <label for="review" class="block mt-4 text-sm font-medium text-gray-700">Want to expand on anything?</label>
-          <div class="mt-1">
-            <textarea rows="3" name="review" id="review" class="block p-2 border-2 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" />
+            <div>
+              <label for="review" class="block mt-4 text-sm font-medium text-gray-700">Want to expand on anything?</label>
+              <div class="mt-1">
+                <textarea rows="3" name="review" id="review" class="block p-2 border-2 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" />
+              </div>
+            </div>
+
+            <div class="flex w-full align-items-end justify-end">
+              <button type="submit" class="inline-flex items-center mt-4 rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                <EnvelopeIcon class="-ml-1 mr-3 h-5 w-5" aria-hidden="true" />
+                Submit daily review
+              </button>
+            </div>
           </div>
-        </div>
-
-        <div class="flex w-full align-items-end justify-end">
-          <button type="submit" class="inline-flex items-center mt-4 rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-            <EnvelopeIcon class="-ml-1 mr-3 h-5 w-5" aria-hidden="true" />
-            Submit daily review
-          </button>
+        </form>
+      </div>
+      <div v-else>
+        <div class="flex flex-col align-center justify-center h-full p-8">
+          <h1 class="text-center text-3xl font-bold">Thanks for reviewing today!</h1>
+          <p class="text-center text-md block mt-6">Pull in data from Notion here to show trends here.</p>
         </div>
       </div>
-    </form>
+    </div>
   </main>
 </template>

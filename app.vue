7262a4808c49eb7c3d72567298003cc5343a4746
Rob Erskine <rob@roberskine.com>
2023-01-19 12:32:56 -0500

    updating month selection to match what is in notion

--- app.vue
+++ app.vue
@@ -16,7 +16,8 @@ export default {
   methods: {
     currentDate() {
       const current = new Date();
-      const date = `${current.getFullYear()}-${current.getMonth()+1}-${current.getDate()}`;
+      const months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+      const date = `${current.getFullYear()}-${months[current.getMonth()]}-${current.getDate()}`;
       return date;
     },
     encode(data) {
